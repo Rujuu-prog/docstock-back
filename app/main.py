535411b22e314179cb5f637c0c3b0de69ec6e5f6
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-04-03 17:28:55 +0900

    :pencil2: fix to async def from def

--- app/main.py
+++ app/main.py
@@ -13,11 +13,11 @@ app = FastAPI()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+async def hash_password(password: str):
     return pwd_context.hash(password)
 
 
-def get_db():
+async def get_db():
     db = SessionLocal()
     try:
         yield db
@@ -25,7 +25,7 @@ def get_db():
         db.close()
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if not is_valid_email(user.email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid email format")
     # Check if a user with the same name or email already exists
@@ -42,58 +42,58 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def get_user(user_id: int, db: Session = Depends(get_db)):
+async def get_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return db_user
 
 @app.post("/groups/", response_model=schemas.Group)
-def create_group(group: schemas.GroupCreate, db: Session = Depends(get_db)):
+async def create_group(group: schemas.GroupCreate, db: Session = Depends(get_db)):
     return crud.create_group(db=db, group=group)
 
 @app.get("/groups/{group_id}", response_model=schemas.Group)
-def get_group(group_id: int, db: Session = Depends(get_db)):
+async def get_group(group_id: int, db: Session = Depends(get_db)):
     db_group = crud.get_group(db=db, group_id=group_id)
     if db_group is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found")
     return db_group
 
 @app.post("/roles/", response_model=schemas.Role)
-def create_role(role: schemas.RoleCreate, db: Session = Depends(get_db)):
+async def create_role(role: schemas.RoleCreate, db: Session = Depends(get_db)):
     return crud.create_role(db=db, role=role)
 
 @app.get("/roles/{role_id}", response_model=schemas.Role)
-def get_role(role_id: int, db: Session = Depends(get_db)):
+async def get_role(role_id: int, db: Session = Depends(get_db)):
     db_role = crud.get_role(db=db, role_id=role_id)
     if db_role is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Role not found")
     return db_role
 
 @app.post("/documents/", response_model=schemas.Document)
-def create_document(document: schemas.DocumentCreate, owner_id: int, group_id: int, db: Session = Depends(get_db)):
+async def create_document(document: schemas.DocumentCreate, owner_id: int, group_id: int, db: Session = Depends(get_db)):
     return crud.create_document(db=db, document=document, owner_id=owner_id, group_id=group_id)
 
 @app.get("/documents/{document_id}", response_model=schemas.Document)
-def get_document(document_id: int, db: Session = Depends(get_db)):
+async def get_document(document_id: int, db: Session = Depends(get_db)):
     db_document = crud.get_document(db=db, document_id=document_id)
     if db_document is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Document not found")
     return db_document
 
 @app.post("/tags/", response_model=schemas.Tag)
-def create_tag(tag: schemas.TagCreate, document_id: int, db: Session = Depends(get_db)):
+async def create_tag(tag: schemas.TagCreate, document_id: int, db: Session = Depends(get_db)):
     return crud.create_tag(db=db, tag=tag, document_id=document_id)
 
 @app.get("/tags/{tag_id}", response_model=schemas.Tag)
-def get_tag(tag_id: int, db: Session = Depends(get_db)):
+async def get_tag(tag_id: int, db: Session = Depends(get_db)):
     db_tag = crud.get_tag(db=db, tag_id=tag_id)
     if db_tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found")
     return db_tag
 
 @app.post("/user_group_roles/")
-def add_user_to_group(user_id: int, group_id: int, role_id: int, db: Session = Depends(get_db)):
+async def add_user_to_group(user_id: int, group_id: int, role_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
