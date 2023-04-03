ca6d51c46cbd75e9103e54354a1b192fb96859ed
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-04-03 22:46:20 +0900

    :recycle: refactor test code

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, drop_database
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from app.database import Base
 
 @pytest.fixture(scope="function")
@@ -10,9 +10,9 @@ def SessionLocal():
     TEST_SQLALCHEMY_DATABASE_URL = "sqlite:///./test_temp.db"
     engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-    assert not database_exists(TEST_SQLALCHEMY_DATABASE_URL), "Test database already exists. Aborting tests."
-
     # Create test database and tables
+    if not database_exists(TEST_SQLALCHEMY_DATABASE_URL):
+        create_database(TEST_SQLALCHEMY_DATABASE_URL)
     Base.metadata.create_all(engine)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
